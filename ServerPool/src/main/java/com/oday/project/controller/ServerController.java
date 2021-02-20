d9b251421fce8b34ccbd2633c4cdf7eab2e3a92b
OdayYameen <oday.yameen@exalt.email>
2021-02-20 19:08:20 +0200

    change behavior of controller of server pool

--- ServerPool/src/main/java/com/oday/project/controller/ServerController.java
+++ ServerPool/src/main/java/com/oday/project/controller/ServerController.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,30 +43,37 @@ public class ServerController {
 
 	@RequestMapping(value = "/getMyServer/{id}",headers = "Accept=application/json")
 	public Server getMyServer(@PathVariable(value  = "id") long id) {
-		
-		return serverRepository.findById(id).get();
+		Server ser=serverRepository.findById(id).get();
+		if(ser.getStatus()==ServerStatus.Active) {
+			return ser;
+		}else return null;
+	}
+
+	@RequestMapping(value = "/getString/{str}",headers = "Accept=application/json")
+	public String getString(@PathVariable(value = "str")String str){
+		return "{message:"+str+"}";
 
 	}
 
 	@RequestMapping(value = "/allocate/{capacity}/{nameOfUser}",headers = "Accept=application/json")
-	public String allocate(@PathVariable(value = "capacity") int capacity,
+	public RedirectView allocate(@PathVariable(value = "capacity") int capacity,
 			@PathVariable(value = "nameOfUser") String nameOfUser) {
 		if (capacity > 100)
-			return "Your capacity grater than 100";
+			return new RedirectView("/getString/Your capacity grater than 100");
 		
 		String string= allocateServer( capacity,nameOfUser);
-		return string;
+		return new RedirectView(string);
 		
 		
 	}
-	private String allocateServer(int capacity, String nameOfUser) {
+
+	String allocateServer(int capacity, String nameOfUser) {
 		List<Server> servers = new ArrayList<Server>();
 		serverRepository.findAll().forEach(servers::add);
 
 		Server s = getBestServer(servers, capacity);
 
-		s.setStatus(ServerStatus.Createing);
-		 System.out.println("the server with id= " +s.getId()+" now is in = "+s.getStatus());
+
 
 		s.getMyUser().add(nameOfUser);
 
@@ -86,26 +95,8 @@ public class ServerController {
 			
 		}
 	
-			Thread thread=new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-				try {
-					Thread.sleep(12000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-					
-				s.setStatus(ServerStatus.Active);
-				 System.out.println("the server with id= " +s.getId()+" now is in = "+s.getStatus());
-				serverRepository.save(s);
-					
-				}
-			});
-					
-			thread.start();
-		return "you will take the server after 12 sec , your server id ="+s.getId();
+			return "/getMyServer/"+s.getId();
+
 		
 	}
 
@@ -149,8 +140,31 @@ public class ServerController {
 			long newid = (System.currentTimeMillis() << 20) | (System.nanoTime() & 0xFFFFFL);
 			List<String> l = new ArrayList<String>();
 			l.add("");
-			Server nnsServer = new Server(newid, 0, ServerStatus.Active, 0, l, 1);
+			Server nnsServer = new Server(newid, 0, ServerStatus.Createing, 0, l, 1);
+			System.out.println("the server with id= " +nnsServer.getId()+" now is in = "+nnsServer.getStatus());
+
 			serverRepository.save(nnsServer);
+			Thread thread=new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						Thread.sleep(20000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					nnsServer.setStatus(ServerStatus.Active);
+					System.out.println("the server with id= " +nnsServer.getId()+" now is in = "+nnsServer.getStatus());
+					serverRepository.save(nnsServer);
+
+				}
+			});
+
+			thread.start();
+
+
 			return nnsServer;
 		}
 
